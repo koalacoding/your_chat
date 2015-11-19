cacc2af266da1a27f73e060f92fc409996f443df
koalacoding <koalacoding@gmail.com>
2015-11-19 01:16:12 +0100

    Center MainWindow in the middle of the screen

--- your_chat/mainwindow.cpp
+++ your_chat/mainwindow.cpp
@@ -1,3 +1,6 @@
+#include "QRect"
+#include "QDesktopWidget"
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -6,6 +9,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Centers the window in the middle of the screen.
+    QRect position = frameGeometry();
+    position.moveCenter(QDesktopWidget().availableGeometry().center());
+    move(position.topLeft());
 }
 
 MainWindow::~MainWindow()
